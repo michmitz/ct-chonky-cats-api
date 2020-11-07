edc51dbfde4f7d2148ed0297662c82a8e36f7ae6
michmitz <stermitzm@gmail.com>
2020-11-06 16:49:23 -0800

    Added chonky post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,25 @@ describe('chonky-cat-api-backend routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('creates a chonk', () => {
+    return request(app)
+      .post('/chonks') 
+      .send({
+        name: 'Cinderblock',
+        weight: 10,
+        description: 'Insta-famous cat that is in much better shape these days.',
+        imageUrl: 'https://cinderblock.com/cinderblock.png'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'Cinderblock',
+          weight: '10',
+          description: 'Insta-famous cat that is in much better shape these days.',
+          imageUrl: 'https://cinderblock.com/cinderblock.png'
+        });
+      });
+  }); 
 });
+
