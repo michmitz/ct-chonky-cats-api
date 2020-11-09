8b375d6e615aa789742e8c33f4f8a07d68ddfa53
michmitz <stermitzm@gmail.com>
2020-11-08 20:21:56 -0800

    Added get all route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Chonk = require('../lib/models/chonk');
 
 describe('chonky-cat-api-backend routes', () => {
   beforeEach(() => {
@@ -26,6 +27,39 @@ describe('chonky-cat-api-backend routes', () => {
           imageUrl: 'https://cinderblock.com/cinderblock.png'
         });
       });
-  }); 
+  });
+
+  it('gets all chonks', async() => {
+    const chonks = await Promise.all([
+      {
+        name: 'Cinderblock',
+        weight: '10',
+        description: 'Insta-famous cat that is in much better shape these days.',
+        imageUrl: 'https://cinderblock.com/cinderblock.png'
+      },
+      {
+        name: 'Momo',
+        weight: '50',
+        description: 'Momo is an overweight cat with a love of treats.',
+        imageUrl: 'https://momo.com/momo.png'
+      },
+      {
+        name: 'Turnip',
+        weight: '35',
+        description: 'Turnip knows how to party!',
+        imageUrl: 'https://turnip.com/turnip.png'
+      }
+    ].map(chonk => Chonk.insert(chonk)));
+
+    return request(app)
+      .get('/chonks')
+      .then(res => {
+        chonks.forEach(chonk => {
+          expect(res.body).toContainEqual(chonk);
+        });
+      });
+  });
 });
+
+
 
